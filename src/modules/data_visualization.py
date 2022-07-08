96442e15a2fdf27b3eeca1bf73dcd9d3cf5bb771
Gianluca Fuoco <gianluca_1227@hotmail.com>
2022-07-08 11:58:22 -0400

    created module to plot stock data

---
+++ src/modules/data_visualization.py
@@ -0,0 +1,23 @@
+import seaborn as sns
+from pandas import to_datetime
+from datetime import date
+import matplotlib.pyplot as plt
+from matplotlib.dates import YearLocator, ConciseDateFormatter
+
+
+sns.set_theme(style="darkgrid", context="talk")
+
+
+def plot_stock(df, start: date = None):
+    df["Date"] = to_datetime(df["Date"]).dt.to_pydatetime()
+    locator = YearLocator(3)
+    formatter = ConciseDateFormatter(locator)
+    _, ax = plt.subplots(figsize=(16, 8))
+    ax.xaxis.set_major_locator(locator)
+    ax.xaxis.set_major_formatter(formatter)
+    ax.tick_params(axis="x", labelrotation=45)
+    if start is not None:
+        ax.set_xlim(start, df["Date"].max())
+    sns.lineplot(ax=ax, data=df, x=df["Date"], y="Close")
+
+    plt.show()
