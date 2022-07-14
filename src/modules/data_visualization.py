53952021056f0e96a51891ae630cb18765510114
Gianluca Fuoco <gianluca_1227@hotmail.com>
2022-07-14 15:18:24 -0400

    added function to plot predicted values against real values added function to plot loss

--- src/modules/data_visualization.py
+++ src/modules/data_visualization.py
@@ -2,10 +2,45 @@ import seaborn as sns
 from pandas import to_datetime
 from datetime import date
 import matplotlib.pyplot as plt
+import pandas as pd
 from matplotlib.dates import YearLocator, ConciseDateFormatter
 
 
 sns.set_theme(style="darkgrid", context="talk")
+
+
+def plot_loss(history):
+    """
+    Plot the loss history from the neural network
+
+    history: history callback from model.fit
+    """
+    _fig, ax = plt.subplots(figsize=(16, 8))
+    loss = history.history["loss"]
+    val_loss = history.history["val_loss"]
+    df = pd.DataFrame()
+    df["loss"] = loss
+    df["val_loss"] = val_loss
+    sns.lineplot(data=df, palette=["r", "b"], ax=ax)
+    ax.set_xlabel("Epochs")
+    ax.set_ylabel("Loss (MSE)")
+    ax.set_title("Loss History")
+    plt.show()
+
+
+def plot_prediction(y_pred_unscaled, y_test_unscaled):
+    _, ax = plt.subplots(figsize=(16, 8))
+    df = pd.DataFrame()
+    df["actual"] = y_test_unscaled.reshape(-1)
+    df["predicted"] = y_pred_unscaled.reshape(-1)
+    sns.lineplot(data=df, palette=["r", "b"], ax=ax)
+    ax.set_title("Predicted Stock Price vs Actual Price")
+    ax.set_ylabel("Price USD")
+    ax.set_xlabel("Days")
+    ax.set_xticklabels([])
+    ax.set_yticklabels([])
+    ax.legend()
+    plt.show()
 
 
 def plot_stock(df, start: date = None):
